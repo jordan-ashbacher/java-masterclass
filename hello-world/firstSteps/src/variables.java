f4d7a74731fc97ef3df0f98cdb4a80c421fed6aa
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-05-03 21:10:50 -0500

    debugging java terminal

--- hello-world/firstSteps/src/variables.java
+++ hello-world/firstSteps/src/variables.java
@@ -1,5 +1,9 @@
-class QuickStart {
+public class variables {
     public static void main(String[] args) {
         System.out.println("Hello, World.");
+
+        int num1 = 5;
+
+        System.out.println(num1);
     }
 }
