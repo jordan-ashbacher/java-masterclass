37bfacf753e40370494764c836536b370fdf0b11
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-05-06 12:37:02 -0500

    min and max values of float and double

---
+++ firstSteps/floatanddouble.java
@@ -0,0 +1,18 @@
+public class floatanddouble {
+    public static void main(String[] args) {
+        System.out.println("in float and double file");
+
+        float minFloat = Float.MIN_VALUE;
+        float maxFloat = Float.MAX_VALUE;
+        System.out.println("Float min value = " + minFloat);
+        System.out.println("Float max value = " + maxFloat);
+
+        double minDouble = Double.MIN_VALUE;
+        double maxDouble = Double.MAX_VALUE;
+        System.out.println("Double min value = " + minDouble);
+        System.out.println("Double max value = " + maxDouble);
+        
+
+        
+    }
+}
