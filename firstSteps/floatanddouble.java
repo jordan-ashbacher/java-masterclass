531c938ebf78c6f728f1db68a6294b86c96a9398
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-05-06 12:49:04 -0500

    completes double and float challenge

--- firstSteps/floatanddouble.java
+++ firstSteps/floatanddouble.java
@@ -12,9 +12,17 @@ public class floatanddouble {
         System.out.println("Double min value = " + minDouble);
         System.out.println("Double max value = " + maxDouble);
         
-        int intValue = 5;
-        float floatValue = 5.25f;
-        double doubleValue = 5.25d;
+        int intValue = 5 / 3;
+        float floatValue = 5f / 3f;
+        double doubleValue = 5d / 3d;
+        
+        System.out.println("int value = " + intValue);
+        System.out.println("float value = " + floatValue);
+        System.out.println("double value = " + doubleValue);
+
+        double pound = 175;
+        double weightInKilo = pound * 0.45359237;
+        System.out.println("175lbs in kilograms = " + weightInKilo);
 
     }
 }
