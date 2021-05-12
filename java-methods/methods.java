4406ddbaa20edc6522b68df1e41ec61da356b798
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-05-12 14:48:11 -0500

    creates java method to calculate total score

---
+++ java-methods/methods.java
@@ -0,0 +1,18 @@
+public class methods {
+    public static void main (String[] args) {
+        calculateScore();
+    }
+
+    public static void calculateScore() {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score is " + finalScore);
+        }
+    }
+}
