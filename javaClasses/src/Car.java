388ee1a4487495f7934b7ea106bd9547f564b414
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-05-14 15:42:28 -0500

    creates classes project and troubleshoot sourcepath issue

---
+++ javaClasses/src/Car.java
@@ -0,0 +1,7 @@
+public class Car {
+    private int doors;
+    private String make;
+    private String model;
+    private String year;
+    private String color;
+}
---
+++ javaClasses/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        porsche.
+    }
+}
