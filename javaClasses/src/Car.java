74b387e367131efdb0e3fd2b3b0952f9a9c7b362
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-05-17 14:37:06 -0500

    adds string validation to class method

--- javaClasses/src/Car.java
+++ javaClasses/src/Car.java
@@ -6,6 +6,11 @@ public class Car {
     private String color;
 
     public void setModel(String model) {
-        this.model = model;
+        String validModel = model.toLowerCase();
+        if (validModel.equals("carrera") || validModel.equals("911")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
     }
 }
